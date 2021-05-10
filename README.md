fa4998e6166020f576477956e4ae68f8adef51b6
valeriomoroni95 <valeriomoroni95@gmail.com>
2021-05-10 12:37:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JcsTests
+ JCS Parameterized Tests
---
+++ src/main/java/it/uniroma2/dicii/isw2/jcs/paramTests/App.java
@@ -0,0 +1,13 @@
+package it.uniroma2.dicii.isw2.jcs.paramTests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
